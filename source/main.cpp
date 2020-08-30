0f2e68a8abcdfe04e857297bbf360f9683ffcb34
mattbrownca <6611786+MattBrownCA@users.noreply.github.com>
2020-08-30 14:52:21 -0700

    Trying to get the IRQ handler working

--- source/main.cpp
+++ source/main.cpp
@@ -14,23 +14,14 @@
 #include "em_cmu.h"
 #include "em_emu.h"
 #include "em_gpio.h"
-#include "em_rtc.h"
+#include "em_rtcc.h"
 #include "em_i2c.h"
 
 static uint64_t g_ticks = 0;
 
-/**************************************************************************//**
- * @brief Delays number of msTick Systicks (typically 1 ms)
- * @param dlyTicks Number of ticks to delay
- *****************************************************************************/
-void TIMER0_IRQHandler()
-{
-    g_ticks ++;
-
-    if(g_ticks % 1000 == 0)
-    {
-        GPIO_PinOutToggle(LED_PORT, LED_PIN);
-    }
+void RTCC_IRQHandler() {
+    ++g_ticks;
+    RTCC_StatusClear();
 }
 
 /**************************************************************************//**
@@ -38,19 +29,16 @@ void TIMER0_IRQHandler()
  *****************************************************************************/
 int main(void)
 {
-    I2C_Init_TypeDef t2c_init = I2C_INIT_DEFAULT;
-
-    I2C_Init()
+    RTCC_Init_TypeDef rtcc_init = RTCC_INIT_DEFAULT;
 
     CHIP_Init();
 
-    /* Initialize LED driver */
-    GPIO_PinModeSet(LED_PORT, LED_PIN, gpioModePushPull, 0);
-    GPIO_PinOutSet(LED_PORT, LED_PIN);
+    RTCC_Init(&rtcc_init);
+    RTCC_StatusClear();
 
-    CMU_IntClear(~0);
-    CMU_IntEnable(TIMER1_IRQn);
-    CMU_ClockEnable(cmuClock_TIMER1, true);
+    CMU_IntEnable(RTCC_IRQn);
+
+    RTCC_Enable(true);
 
 
     for(;;)
