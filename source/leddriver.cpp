a86d2367a4e28e5537ebc0dcd045b671ec2c1b04
1BADragon <6611786+MattBrownCA@users.noreply.github.com>
2020-08-31 19:00:17 -0700

    Added some more

--- source/leddriver.cpp
+++ source/leddriver.cpp
@@ -1,4 +1,5 @@
 #include "leddriver.h"
+#include "i2cspm.h"
 
 LEDDriver::LEDDriver()
     : I2CComponent()
@@ -8,11 +9,15 @@ LEDDriver::LEDDriver()
 
 void LEDDriver::set_led(uint32_t led_num, bool val)
 {
+    I2C_TransferSeq_TypeDef seq;
     if (led_num >= LED_COUNT) {
         return;
     }
 
     // construct an i2c message
+
+
+    send(&seq);
 }
 
 void LEDDriver::toggle_led(uint32_t led_num)
