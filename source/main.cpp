1638f155109349f10c0d61c61c124077d1ccf2d4
mattbrownca <6611786+MattBrownCA@users.noreply.github.com>
2020-09-07 17:27:29 -0700

    idk

--- source/main.cpp
+++ source/main.cpp
@@ -1,57 +1,13 @@
-#include <stddef.h>
-#include "rtcdriver.h"
-#include "leddriver.h"
 
-#include "native_gecko.h"
 
-#include "em_chip.h"
-#include "em_emu.h"
-#include "em_cmu.h"
+#include "gecko_configuration.h"
+#include "rtos_gecko.h"
 
-static LEDDriver *led_driver;
+static gecko_configuration_t gecko_config = {
 
-static RTCDRV_TimerID_t id;
-static bool on = 0;
-static bool triggered = false;
+};
 
-void myCallback( RTCDRV_TimerID_t id, void * user )
-{
-    (void) user; // unused argument in this example
-    (void) id;
-
-    triggered = true;
-}
-
-int main( void )
-{
-    gecko_configuration_t gecko_config;
-    memset(&gecko_config, 0, sizeof(gecko_config));
+int main() {
 
     gecko_init(&gecko_config);
-
-
-    CHIP_Init();
-    RTCDRV_Init();
-
-    LEDDriver _main_led_driver;
-    led_driver = &_main_led_driver;
-
-    RTCDRV_AllocateTimer( &id );
-    RTCDRV_StartTimer( id, rtcdrvTimerTypePeriodic, 5000, myCallback, NULL );
-
-    for (;;) {
-        EMU_EnterEM1();
-
-        if (triggered) {
-            if (on) {
-                led_driver->global_on(false);
-                on = false;
-            } else {
-                led_driver->global_off(false);
-                on = true;
-            }
-            triggered = false;
-        }
-    }
-    return 0;
 }
